2332a927eefd74c05b7af9d1f269822d1c5a5348
Vasanti-Vaghela <vasantivaghela2812@gmail.com>
2025-02-22 19:33:40 +0530

    use useReducer for simple count increment decrement

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,14 +1,37 @@
-import { createContext } from "react";
-import Parent from "./components/Parent";
-import ThemeProvider from "./context/themeContext";
+import { useReducer } from "react";
 
-export const ThemeContext = createContext();
+const ACTIONS = {
+  INCREMENT: "increment",
+  DECREMENT: "decrement",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 };
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  const increment = () => {
+    dispatch({ type: ACTIONS.INCREMENT });
+  };
+
+  const decrement = () => {
+    dispatch({ type: ACTIONS.DECREMENT });
+  };
   return (
-    <ThemeProvider>
-      <Parent />
-    </ThemeProvider>
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
   );
 }
 
